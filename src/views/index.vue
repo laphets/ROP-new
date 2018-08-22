f770a2feda0b7524eb0a9f47ddda6d393c24de72
fish98 <jiongchiyu@gmail.com>
2018-08-23 07:37:28 +0800

    add index.vue

---
+++ src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    <div>Welcome to 2018 QSC Autumn Recruit DashBoard</div>
+    <img src='https://sea.zjuqsc.com/thumbnail/6787bde0-e4c0-4392-8429-621c43e0a0e4/1024/6332532_p0.png'>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component
+export default class Home extends Vue {}
+</script>
+
+<style>
+.index{
+    text-align: center;
+    font-weight: bold;
+    font-size: 40px
+}
+</style>
