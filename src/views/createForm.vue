280b5f981158fb82ee2e82e2a989a32d3c40d4ff
fish98 <jiongchiyu@gmail.com>
2018-08-22 12:16:20 +0800

    fetch init in createForm.vue && fix some package and tslint bugs

--- src/views/createForm.vue
+++ src/views/createForm.vue
@@ -13,18 +13,22 @@
             >
             </a-input-search>
 
-                <div v-for="item in renderData" :key="item.title">
+                <div v-for="item in renderData" :key="item.ID">
                     <a-card
                     class="card"
                     :bordered="false"
                     hoverable
                     style="width: 250px;"
                     >   
-                        <div class="card_title">{{item.title}}</div> 
+                        <div class="card_title">{{item.data.name}}</div> 
                         <hr class="card_line">
                         <div class="card_info">
+                            
+                            <!-- These two components below is not into use !!! -->
                             <p class="card_info_detail">{{item.info}}</p>
                             <p class="card_edit_time">{{item.time}}</p>
+                            <!--  -->
+                        
                         </div>
                         <ul class="ant-card-actions" slot="actions">
                             <li style="width: 25%;"><a-icon type="star-o" @click="star" /></li>
@@ -42,32 +46,51 @@
 </template>
 
 <script lang="ts">
-const dataSource = [
-{
-    id: 1,
-    title: '2018春纳报名表',
-    info: '这是求是潮2018春季纳新报名表',
-    time: 'Last Edit Time is 2017.12.1 20:35'
-}, 
-{
-    id: 2,
-    title: '跳跳鱼',
-    info: '跳跳鱼啦啦啦啦啦啦',
-    time: 'Last Edit Time is 2008.1.1 23:12'
+// const dataSource1 = [
+// {
+//     id: 1,
+//     title: '2018春纳报名表',
+//     info: '这是求是潮2018春季纳新报名表',
+//     time: 'Last Edit Time is 2017.12.1 20:35'
+// }, 
+// {
+//     id: 2,
+//     title: '跳跳鱼',
+//     info: '跳跳鱼啦啦啦啦啦啦',
+//     time: 'Last Edit Time is 2008.1.1 23:12'
  
-}, {
-    id: 3,
-    title: '裸犇的报名表',
-    info: '哈哈哈哈哈哈哈啊哈哈哈哈哈',
-    time: 'Last Edit Time is 1908.3.6 08:48'
-}]
+// }, {
+//     id: 3,
+//     title: '裸犇的报名表',
+//     info: '哈哈哈哈哈哈哈啊哈哈哈哈哈',
+//     time: 'Last Edit Time is 1908.3.6 08:48'
+// }]
 
 import { Component, Vue, Watch } from 'vue-property-decorator';
+import { defineLocale } from 'moment';
+
+// backend api url 
+
+const site: string = 'http://101.132.66.238/api/v1/form'
+
 @Component
 export default class InstancePageClass extends Vue {
             
-    renderData: object[] = dataSource
+    renderData: object[] = [] 
+    // on mount function
 
+    dataSource: object[] = []
+
+    async mounted() {
+        const dataString: string = await fetch(site).then(res => res.text())
+        const dataSource: object[] = JSON.parse(dataString).data
+        dataSource.map((item: any, index: number) => {
+            item.data = JSON.parse(item.data)
+        })
+        this.dataSource = dataSource
+        this.renderData = dataSource
+    }
+  
     // test function
 
     fish() {
@@ -95,11 +118,10 @@ export default class InstancePageClass extends Vue {
     // bind search function 
 
     sort(value: string) {
-        let word = value
+        let word: string = value
         let newData: object[] = []
-        console.log(this.renderData)
-        dataSource.map((item, index) => {
-            if (!item.title.indexOf(word)) {
+        this.dataSource.map((item: any, index: number) => {
+            if (item.name.indexOf(word ) > -1) {
                 newData.push(item)
             }
         })
@@ -116,7 +138,7 @@ export default class InstancePageClass extends Vue {
 .working_area{
     background-color: white;
     height: 900px;
-    width: 700px;
+    width: 65%;
     margin-left: 10%;
     margin-top: 5%;
     box-shadow: 4px 4px 4px rgb(145, 145, 145)
