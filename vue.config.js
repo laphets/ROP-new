2e3ecc1fb361bfdb3a60636765f9f554ccf001ba
laphets <i@laphets.com>
2018-08-28 13:48:29 +0800

    Modify host config

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
     port: 80,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3000',
+        target: 'https://rop.zjuqsc.com/api',
         pathRewrite: {
           '^/api': '/'
         }
