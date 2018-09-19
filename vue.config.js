6d0d1c9b1c0312ccca56386a9ee5b183051a51dd
laphets <i@laphets.com>
2018-09-19 22:10:26 +0800

    Add ; for return

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].chunksSortMode = 'none'
-      return args
+      return args;
     })
   },
   devServer: {
