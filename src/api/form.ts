3be7db47ebc2f25e30fc702001ea2d3996f948c5
laphets <i@laphets.com>
2019-02-04 23:40:17 +0800

    Add apis for form create and update

--- src/api/form.ts
+++ src/api/form.ts
@@ -3,3 +3,11 @@ import request from '@/utils/request'
 export const getFormList = ()  => {
     return request.get('v1/form')
 }
+
+export const createForm = (data: any) => {
+    return request.post('v1/form', data)
+}
+
+export const updateForm = (id: number, data: any) => {
+    return request.put(`v1/form/${id}`, data)
+}
